2545dfc434aae2a54ac1d9ac9bba427be9f5b971
christian19782013 <christian19782013@gmail.com>
2015-10-10 07:22:59 -0500

    D

---
+++ AUDITORIA_SISTEMAS.mm
@@ -0,0 +1,383 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1443877744135" ID="ID_1142116578" MODIFIED="1443881279290">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      ADMINISTRACI&#211;N Y AUDITORIA DE SISTEMAS
+    </p>
+    <ul>
+      <li>
+        Geovanny Roldan
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+<node CREATED="1443881279063" ID="ID_656966611" MODIFIED="1443881468589" POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Preguntas sobre objetivos
+    </p>
+  </body>
+</html></richcontent>
+<font NAME="SansSerif" SIZE="24"/>
+<node CREATED="1443880848314" ID="ID_408857251" MODIFIED="1443881665241">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      MODELOS DE MADURES
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1443881295162" ID="ID_1587209606" MODIFIED="1443881330802">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Modelos de madurez de los procesos
+    </p>
+    <p>
+      Existen escalas de 1 a 5
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1443881334138" ID="ID_1544637223" MODIFIED="1443881343879" TEXT="">
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1443881335193" ID="ID_820047012" MODIFIED="1443881346667" TEXT="">
+<icon BUILTIN="full-2"/>
+</node>
+<node CREATED="1443881336156" ID="ID_1992223178" MODIFIED="1443881349890" TEXT="">
+<icon BUILTIN="full-3"/>
+</node>
+<node CREATED="1443881336779" ID="ID_790436442" MODIFIED="1443881353807" TEXT="">
+<icon BUILTIN="full-4"/>
+</node>
+<node CREATED="1443881337222" ID="ID_7385076" MODIFIED="1443881357622" TEXT="">
+<icon BUILTIN="full-5"/>
+</node>
+</node>
+</node>
+<node CREATED="1443881405626" ID="ID_1014030" MODIFIED="1443881413013" TEXT="ISO 17799">
+<node CREATED="1443881691050" HGAP="167" ID="ID_858827799" MODIFIED="1443882014815" VSHIFT="45">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      ISO 17000 SON MARCOS DE REFERENCIAS O BUENAS PR&#193;CTICAS
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      LA ISO 27000 SE DIFERENCIA QUE ES CERTIFICABLE
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      NORMAS QUE SE DEBEN CUMPLIR PARA LA SEGURIDAD DE LA INFORMACI&#211;N
+    </p>
+  </body>
+</html></richcontent>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_858827799" ENDARROW="Default" ENDINCLINATION="447;0;" ID="Arrow_ID_253364326" SOURCE="ID_1806776500" STARTARROW="None" STARTINCLINATION="447;0;"/>
+<node COLOR="#990000" CREATED="1443881815245" HGAP="70" ID="ID_1084068674" MODIFIED="1443881868381" VSHIFT="86">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      SEGURIDAD DE LA INFORMACI&#211;N
+    </p>
+  </body>
+</html></richcontent>
+<font NAME="SansSerif" SIZE="10"/>
+</node>
+<node CREATED="1443881823795" HGAP="68" ID="ID_1225514238" MODIFIED="1443881841149" TEXT="SEGURIDAD TECNOL&#xd3;GICA DE LA INFO LA MANEJA TICS" VSHIFT="118"/>
+</node>
+</node>
+<node CREATED="1443881413557" ID="ID_1806776500" MODIFIED="1443882014815" TEXT="RIESGOS COBIT ISO 27301">
+<arrowlink DESTINATION="ID_858827799" ENDARROW="Default" ENDINCLINATION="447;0;" ID="Arrow_ID_253364326" STARTARROW="None" STARTINCLINATION="447;0;"/>
+<node CREATED="1443881880850" HGAP="272" ID="ID_777886000" MODIFIED="1443881903861" VSHIFT="81">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      SE HABLAR&#193; COMO UNO DE LOS PROCESOS QUE DEBE MANEJAR TICS
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1443881492036" ID="ID_238675236" MODIFIED="1443881521187">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      ALCANCE DE LA AUDITORIA
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1443881952794" HGAP="278" ID="ID_1287356740" MODIFIED="1443882013806" VSHIFT="29">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      AUDITORIAS QUE VIENEN DIRECTO A LOS GERENTES
+    </p>
+    <p>
+      O PUEDEN IR A OTRA &#193;REA Y TERMINAR EN TICS
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1443881508644" ID="ID_455732923" MODIFIED="1443881534189">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      PROCESOS
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1443882078257" HGAP="367" ID="ID_448221563" MODIFIED="1443882106036" VSHIFT="8">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      ES PARTE FUNDAMENTAL DE COMO SE DEBEN ORAGANIZAR LOS DEP DE TICS PARA PODER ADDMINISTRAR CORRECTAMENTE
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1443881537280" ID="ID_1088326150" MODIFIED="1443881581517">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      EVALUACI&#211;N DE POL&#205;TICAS DELA CONTRALOR&#205;A
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1443882110166" HGAP="184" ID="ID_175698115" MODIFIED="1443882564936" VSHIFT="44">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      DEPENDEN DEL SECTOR EN EL QUE ESTE
+    </p>
+    <p>
+      ESTA DEPENDE DEL SECTOR
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1443881583681" ID="ID_598881699" MODIFIED="1443881596248" TEXT="CUANTAS METODOLOG&#xcd;AS HAY MIX">
+<node CREATED="1443882744136" HGAP="339" ID="ID_1583719315" MODIFIED="1443882916151" VSHIFT="97">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      HAY MUCHAS METODOLOGPIAS
+    </p>
+    <p>
+      ISOS
+    </p>
+  </body>
+</html></richcontent>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_1583719315" ENDARROW="Default" ENDINCLINATION="611;0;" ID="Arrow_ID_888584962" SOURCE="ID_1559599504" STARTARROW="None" STARTINCLINATION="611;0;"/>
+</node>
+</node>
+<node CREATED="1443881598110" ID="ID_272566902" MODIFIED="1443881633177">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      QU&#201; ASPECTOS DE AUDITOR&#205;A DEBE TOMAR EN CUENTA EN UN NUEVO DESARROLLO?
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1443882803158" ID="ID_955007344" MODIFIED="1443882905454">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      TODO PROCESO TIENE SU RIESGO
+    </p>
+    <p>
+      DONDE EST&#193;N LOS RIESGOS?
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1443881636455" ID="ID_1559599504" MODIFIED="1443882916152" TEXT="MEJORES PR&#xc1;CTICAS">
+<arrowlink DESTINATION="ID_1583719315" ENDARROW="Default" ENDINCLINATION="611;0;" ID="Arrow_ID_888584962" STARTARROW="None" STARTINCLINATION="611;0;"/>
+</node>
+</node>
+<node CREATED="1443889012479" ID="ID_1116574647" MODIFIED="1443889065285" POSITION="right" TEXT="Qu&#xe9; es la Auditoria?">
+<node CREATED="1443889065247" ID="ID_1718772842" MODIFIED="1443889074160">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Interna
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1443889026040" HGAP="81" ID="ID_765908744" MODIFIED="1443889057452" VSHIFT="85">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Revisi&#243;n independiente de una actividad determinada
+    </p>
+    <ul>
+      <li>
+        reportan al directorio
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1443889075207" ID="ID_923614770" MODIFIED="1443889078508" TEXT="Externa">
+<node CREATED="1443889079801" HGAP="93" ID="ID_1433923076" MODIFIED="1443889091485" TEXT="Prisewh" VSHIFT="37"/>
+<node CREATED="1443889083485" HGAP="102" ID="ID_10018749" MODIFIED="1443889093811" TEXT="Deloite" VSHIFT="12"/>
+</node>
+</node>
+<node CREATED="1443889127608" ID="ID_1935203098" MODIFIED="1443889129293" POSITION="right" TEXT="Cu&#xe1;l es el objetivo de la Auditoria">
+<node CREATED="1443889130169" ID="ID_574144499" MODIFIED="1443889136435" TEXT="Minimizar riesgos"/>
+</node>
+<node CREATED="1443890553712" ID="ID_1806096319" MODIFIED="1443890604232" POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      T&#233;rminos clave
+    </p>
+    <ul>
+      <li>
+        Riesgos
+      </li>
+      <li>
+        Controles
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+<node CREATED="1443890559609" ID="ID_1317448054" MODIFIED="1443890569278" TEXT="Ejecuci&#xf3;n de buena estrategia"/>
+<node CREATED="1443890569501" ID="ID_1377635330" MODIFIED="1443890575753" TEXT="Riesgos alcanzar objeti"/>
+<node CREATED="1443890579120" ID="ID_611038328" MODIFIED="1443890585781" TEXT="Controles para mitigar"/>
+<node CREATED="1443890608180" ID="ID_427651324" MODIFIED="1443890610927" TEXT="Control">
+<node CREATED="1443890612349" ID="ID_137003785" MODIFIED="1443890622209" TEXT="Separada de procesos operacionales"/>
+<node CREATED="1443890625143" ID="ID_596624065" MODIFIED="1443890634764" TEXT="Efectivo-gesti&#xf3;n de negocios"/>
+</node>
+<node CREATED="1443890650355" ID="ID_794903823" MODIFIED="1443890657769" TEXT="Control interno"/>
+<node CREATED="1443890658209" ID="ID_1824410902" MODIFIED="1443890673970" TEXT="Sistema de Control Interno">
+<node CREATED="1443890677099" ID="ID_839408562" MODIFIED="1443890682410" TEXT="Confiabilidad info"/>
+<node CREATED="1443890682787" ID="ID_1960049418" MODIFIED="1443890686735" TEXT="protecci&#xf3;n">
+<node CREATED="1443890688924" ID="ID_1774128051" MODIFIED="1443890691278" TEXT="fraude"/>
+<node CREATED="1443890691653" ID="ID_1058853785" MODIFIED="1443890697158" TEXT="uso indevido de fondos"/>
+<node CREATED="1443890703976" ID="ID_201040453" MODIFIED="1443890708946" TEXT="Ambiente de control"/>
+<node CREATED="1443890709203" ID="ID_580985223" MODIFIED="1443890715336" TEXT="Valoraci&#xf3;n de riesgos"/>
+</node>
+</node>
+</node>
+<node CREATED="1443893382031" ID="ID_332028775" MODIFIED="1443893393599" POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Pol&#237;ticas
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1443893397783" ID="ID_16104962" MODIFIED="1443893453647" TEXT="En financieras, la politica es la politica de cr&#xe9;dito caso Dinners"/>
+</node>
+<node CREATED="1443907276270" ID="ID_661204778" MODIFIED="1443907285246" POSITION="right" TEXT="Lo que hace que marche un gerente">
+<node CREATED="1443907286137" ID="ID_706988853" MODIFIED="1443907290058" TEXT="HELPDESK"/>
+<node CREATED="1443907290309" ID="ID_1833356938" MODIFIED="1443907296589" TEXT="PROYECTOS"/>
+<node CREATED="1443907296878" ID="ID_1743780696" MODIFIED="1443907312107">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#160;CONTINUIDAD DE SERVICIO
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1443907393099" ID="ID_1881285601" MODIFIED="1443907403524" POSITION="right" TEXT="riesgos en tecnolog&#xed;as">
+<node CREATED="1443907404975" ID="ID_1097749095" MODIFIED="1443907422335">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      FORMALIDAD
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1443907423148" ID="ID_1886089234" MODIFIED="1443907429302" TEXT="COMPLEJIDAD"/>
+<node CREATED="1443907429883" ID="ID_787415457" MODIFIED="1443907435747" TEXT="DESCONOCIMIENTO"/>
+</node>
+<node CREATED="1443975193039" HGAP="26" ID="ID_157363289" MODIFIED="1443975199663" POSITION="right" TEXT="Tres preguntas clave que le har&#xed;a a la persona que le llam&#xf3;." VSHIFT="73">
+<node CREATED="1443975511178" ID="ID_115016275" MODIFIED="1443975513011" TEXT="Definici&#xf3;n del nivel de servicio"/>
+</node>
+<node CREATED="1443975211913" ID="ID_1812272547" MODIFIED="1443975213303" POSITION="right" TEXT="Tres acciones clave que har&#xed;a luego de colgar la llamad">
+<node CREATED="1443975643513" ID="ID_56228090" MODIFIED="1443975663945" TEXT="reportAR  a los niveles superiores"/>
+</node>
+</node>
+</map>
---
+++ COBIT.mm
@@ -0,0 +1,347 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1444292848091" ID="ID_687657591" MODIFIED="1444292856099" TEXT="COBIT">
+<node CREATED="1444293548952" ID="ID_1907553445" MODIFIED="1444297997334" POSITION="right" TEXT="4.1">
+<node CREATED="1444293552745" ID="ID_83360910" MODIFIED="1444293613172">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Areas de Enfoque del Gobierno TI
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1444293614922" ID="ID_426870939" MODIFIED="1444293629303">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Alineaci&#243;n estrat&#233;gca
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1444293668242" ID="ID_168240349" MODIFIED="1444293672837" TEXT="Entrega de valor"/>
+<node CREATED="1444293679322" ID="ID_210675025" MODIFIED="1444293686443" TEXT="Administraci&#xf3;n y recursos"/>
+<node CREATED="1444293692819" ID="ID_1370885718" MODIFIED="1444293700564" TEXT="Administraci&#xf3;n de riesgos"/>
+<node CREATED="1444293707393" ID="ID_747307593" MODIFIED="1444293713943" TEXT="Medici&#xf3;n de desempe&#xf1;o"/>
+</node>
+<node CREATED="1444293891413" ID="ID_1701128399" MODIFIED="1444293896291" TEXT="Productos Cobit">
+<node CREATED="1444293907982" ID="ID_133897485" MODIFIED="1444293923463" TEXT="Administraci&#xf3;n y consejos Ejecutivos"/>
+<node CREATED="1444293936202" ID="ID_141514077" MODIFIED="1444293943720" TEXT="Administraci&#xf3;n de negocio y TI"/>
+<node CREATED="1444293950089" ID="ID_379532919" MODIFIED="1444293976497">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Profesionales de Gobierno, aseguramiento, control y seguridad
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1444297915336" ID="ID_1135360006" MODIFIED="1444297932324">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Roles y Controles
+    </p>
+    <ul>
+      <li>
+        Matriz RACI
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+<node CREATED="1444297576737" ID="ID_1101386343" MODIFIED="1444297779841">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      PC1.-Metas y Objetivos del Proceso
+    </p>
+    <p>
+      PC2.-Propiedad del Proceso
+    </p>
+    <p>
+      PC3.-Proceso Repetible
+    </p>
+    <p>
+      PC4.-Roles y Responsabilidades
+    </p>
+    <p>
+      PC6.-Desempe&#241;o del proceso
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1444297287456" ID="ID_204148370" MODIFIED="1444297339532">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      La gerencia de la empresa necesita tomar decisiones relativas a estos objetivos de control:
+    </p>
+    <p>
+      &#160;
+    </p>
+    <ul>
+      <li>
+        Seleccionando aquellos aplicables.
+      </li>
+      <li>
+        Decidir aquellos que deben implementarse.
+      </li>
+      <li>
+        Elegir como implementarlos (frecuencia, extensi&#243;n, automatizaci&#243;n, etc.)
+      </li>
+      <li>
+        Aceptar el riesgo de no implementar aquellos que podr&#237;an aplicar.
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+<node CREATED="1444296710204" HGAP="9" ID="ID_722788173" MODIFIED="1444296814825" TEXT="DOMINIOS COBIT" VSHIFT="101">
+<node CREATED="1444479371778" ID="ID_1156888761" MODIFIED="1444479501700">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font size="6">&#160;requerimientos de control gen&#233;ricos </font>
+    </p>
+    <ul>
+      <li>
+        PC1 Metas y Objetivos del Proceso
+      </li>
+      <li>
+        PC2 Propiedad del Proceso
+      </li>
+      <li>
+        PC3 Proceso Repetible
+      </li>
+      <li>
+        PC4 Roles y Responsabilidade
+      </li>
+      <li>
+        PC5 Pol&#237;ticas, Planes y Procedimientos
+      </li>
+      <li>
+        PC6 Desempe&#241;o del Proceso
+      </li>
+    </ul>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1444296726645" ID="ID_1176880460" MODIFIED="1444296784560" TEXT="PLANEAR Y ORGANIZAR">
+<icon BUILTIN="full-1"/>
+<node CREATED="1444296929857" HGAP="111" ID="ID_597184958" MODIFIED="1444479229169" TEXT="PO" VSHIFT="-20">
+<node CREATED="1444479229158" ID="ID_891778682" MODIFIED="1444479242973" TEXT="Ejemplo: PO11">
+<node CREATED="1444479043112" ID="ID_1277332698" MODIFIED="1444479047561" TEXT="P">
+<node CREATED="1444479075633" ID="ID_1457933482" MODIFIED="1444479087503" TEXT="PLANIFICAR"/>
+</node>
+<node CREATED="1444479059263" ID="ID_1940572406" MODIFIED="1444479065659" TEXT="O">
+<node CREATED="1444479090148" ID="ID_1696733496" MODIFIED="1444479092956" TEXT="ORGANIZAR"/>
+</node>
+<node CREATED="1444479066500" ID="ID_286677359" MODIFIED="1444479068754" TEXT="1">
+<node CREATED="1444479114900" ID="ID_576558822" MODIFIED="1444479175391">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      N&#218;MERO DE PROCESO
+    </p>
+    <ul>
+      <li>
+        EN EL LIBRO PC1<font size="5">&#160;</font><font color="#cc0000" size="5">Metas y Objetivos del Proceso</font>
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1444479071068" ID="ID_1153811491" MODIFIED="1444479071908" TEXT="1">
+<node CREATED="1444479205015" ID="ID_511792840" MODIFIED="1444479219521" TEXT="N&#xda;MERO DEL OBJETIVO DE CONTROL"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1444296737789" ID="ID_1397524800" MODIFIED="1444296789142" TEXT="ADQUIRIR E IMPLEMENTAR">
+<icon BUILTIN="full-2"/>
+<node CREATED="1444296933813" HGAP="83" ID="ID_245667960" MODIFIED="1444296995220" TEXT="AI" VSHIFT="2"/>
+</node>
+<node CREATED="1444296758928" ID="ID_1385100701" MODIFIED="1444296794075" TEXT="ENTREGAR Y DAR SOPORTE">
+<icon BUILTIN="full-3"/>
+<node CREATED="1444296940523" HGAP="70" ID="ID_1291406051" MODIFIED="1444296991600" TEXT="DS" VSHIFT="38"/>
+</node>
+<node CREATED="1444296771507" ID="ID_297640919" MODIFIED="1444296799019" TEXT="MONITOREAR Y EVALUAR">
+<icon BUILTIN="full-4"/>
+<node CREATED="1444296948851" HGAP="99" ID="ID_1192610623" MODIFIED="1444297015226" TEXT="ME" VSHIFT="44"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1444297997304" ID="ID_968374524" MODIFIED="1444298005306">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Roles y controles
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1444297974130" ID="ID_518742555" MODIFIED="1444297978145" TEXT="RACI">
+<node CREATED="1444297979862" ID="ID_783114620" MODIFIED="1444297981457" TEXT="R">
+<node CREATED="1444298026516" ID="ID_966230965" MODIFIED="1444298030541" TEXT="Responsable"/>
+</node>
+<node CREATED="1444297982108" ID="ID_1627675849" MODIFIED="1444297982874" TEXT="A">
+<node CREATED="1444298032547" ID="ID_433259110" MODIFIED="1444298036209" TEXT="Rinde Cuentas"/>
+</node>
+<node CREATED="1444297985435" ID="ID_9678024" MODIFIED="1444297985973" TEXT="C">
+<node CREATED="1444298050426" ID="ID_518114419" MODIFIED="1444298064369" TEXT="Quien es consultado"/>
+</node>
+<node CREATED="1444297987452" ID="ID_1086502989" MODIFIED="1444297988330" TEXT="I">
+<node CREATED="1444298066392" ID="ID_1394135498" MODIFIED="1444298069574" TEXT="Informado"/>
+</node>
+</node>
+</node>
+<node CREATED="1444420343929" ID="ID_1070329373" MODIFIED="1444420354123" TEXT="Marco de Trabajo Cobit"/>
+<node CREATED="1444447348741" ID="ID_1247391769" MODIFIED="1444447355171" TEXT="CRITERIOS DE INFORMACI&#xd3;N DE COBIT">
+<node CREATED="1444447364058" ID="ID_841745565" MODIFIED="1444447448871" TEXT="LA EFECTIVIDAD">
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1444447371533" ID="ID_1615326280" MODIFIED="1444447451449" TEXT="LA EFICIOENCIA">
+<icon BUILTIN="full-2"/>
+</node>
+<node CREATED="1444447379320" ID="ID_1118145263" MODIFIED="1444447454504" TEXT="LA CONFIDENCIALIDAD">
+<icon BUILTIN="full-3"/>
+</node>
+<node CREATED="1444447385193" ID="ID_1779047056" MODIFIED="1444447457673">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      LA INTEGRIDAD
+    </p>
+  </body>
+</html></richcontent>
+<icon BUILTIN="full-4"/>
+</node>
+<node CREATED="1444447415006" ID="ID_574106629" MODIFIED="1444447460481" TEXT="DISPONIBILIDAD">
+<icon BUILTIN="full-5"/>
+</node>
+<node CREATED="1444447420645" ID="ID_1042472895" MODIFIED="1444447463434" TEXT="CUMPLIMIENTO">
+<icon BUILTIN="full-6"/>
+</node>
+<node CREATED="1444447438837" ID="ID_1331867562" MODIFIED="1444447466151" TEXT="CONFIABILIDAD">
+<icon BUILTIN="full-7"/>
+</node>
+</node>
+<node CREATED="1444448313629" ID="ID_1189318480" MODIFIED="1444448330718">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Los recursos de TI
+    </p>
+    <p>
+      identificados en COBIT
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1444448342374" HGAP="118" ID="ID_650622307" MODIFIED="1444448352211" TEXT="APLICACIONES" VSHIFT="47"/>
+<node CREATED="1444448375200" HGAP="126" ID="ID_1341222514" MODIFIED="1444448386353" TEXT="LA INFORMACI&#xd3;N" VSHIFT="20"/>
+<node CREATED="1444448415080" HGAP="127" ID="ID_1606660265" MODIFIED="1444448425921" TEXT="INFRAESTRUCTURA" VSHIFT="65"/>
+<node CREATED="1444448431568" HGAP="131" ID="ID_264853911" MODIFIED="1444448438434" TEXT="PERSONAS" VSHIFT="42"/>
+</node>
+<node CREATED="1444451177908" ID="ID_757624270" MODIFIED="1444451198192">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Adem&#225;s, COBIT ofrece ejemplos ilustrativos para cada proceso, los cuales no son exhaustivos o preceptivos de:&#160;&#160;
+    </p>
+    <p>
+      &#8226; Entradas y salidas gen&#233;ricas&#160;&#160;
+    </p>
+    <p>
+      &#8226; Actividades y gu&#237;as sobre roles y responsabilidades en una matriz RACI&#160;&#160;
+    </p>
+    <p>
+      &#8226; Metas de actividades clave (las cosas m&#225;s importantes a realizar)&#160;&#160;
+    </p>
+    <p>
+      &#8226; M&#233;tricas&#160;
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1444451418400" ID="ID_1862497840" MODIFIED="1444451421900" TEXT="cONTROLES">
+<node CREATED="1444451423344" ID="ID_1880385736" MODIFIED="1444451429068" TEXT="Generales">
+<node CREATED="1444451455177" ID="ID_1204243787" MODIFIED="1444451490322">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Desarrollo de sistemas&#160;
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1444451504273" ID="ID_1642791147" MODIFIED="1444451505780" TEXT="Administraci&#xf3;n de cambios"/>
+<node CREATED="1444451511877" ID="ID_1092193771" MODIFIED="1444451513372" TEXT="Seguridad "/>
+<node CREATED="1444451517718" ID="ID_1308155938" MODIFIED="1444451519702" TEXT="Operaciones de computo "/>
+</node>
+<node CREATED="1444451429407" HGAP="22" ID="ID_702458548" MODIFIED="1444451623572" VSHIFT="27">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Aplicaci&#243;n
+    </p>
+    <ul>
+      <li>
+        DOMINIO AI
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+<node CREATED="1444451533037" ID="ID_711891215" MODIFIED="1444451534682" TEXT="Integridad (Completitud) "/>
+<node CREATED="1444451542321" ID="ID_1587806095" MODIFIED="1444451548071" TEXT="Precisi&#xf3;n"/>
+<node CREATED="1444451553366" ID="ID_323764409" MODIFIED="1444451554565" TEXT="Validez "/>
+<node CREATED="1444451561116" ID="ID_257864903" MODIFIED="1444451563449" TEXT="Autorizaci&#xf3;n "/>
+<node CREATED="1444451572657" ID="ID_78364360" MODIFIED="1444451578773" TEXT="Segregaci&#xf3;n de funciones"/>
+</node>
+</node>
+</node>
+</node>
+</map>
