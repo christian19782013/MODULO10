4b1c6dd539e5f47f3a7dbce405badd4f0312e807
christian19782013 <christian19782013@gmail.com>
2015-10-11 07:04:57 -0500

    S

--- COBIT.mm
+++ COBIT.mm
@@ -1,7 +1,7 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1444292848091" ID="ID_687657591" MODIFIED="1444292856099" TEXT="COBIT">
-<node CREATED="1444293548952" ID="ID_1907553445" MODIFIED="1444297997334" POSITION="right" TEXT="4.1">
+<node CREATED="1444293548952" ID="ID_1907553445" MODIFIED="1444560921792" POSITION="right" TEXT="4.1">
 <node CREATED="1444293552745" ID="ID_83360910" MODIFIED="1444293613172">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -301,8 +301,10 @@
   </body>
 </html></richcontent>
 </node>
+<node CREATED="1444560920941" ID="ID_799573252" MODIFIED="1444560920941" TEXT="">
 <node CREATED="1444451418400" ID="ID_1862497840" MODIFIED="1444451421900" TEXT="cONTROLES">
-<node CREATED="1444451423344" ID="ID_1880385736" MODIFIED="1444451429068" TEXT="Generales">
+<node CREATED="1444451423344" ID="ID_1880385736" MODIFIED="1444561163738" TEXT="Generales">
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_1880385736" ENDARROW="Default" ENDINCLINATION="567;0;" ID="Arrow_ID_749439136" SOURCE="ID_754201197" STARTARROW="None" STARTINCLINATION="567;0;"/>
 <node CREATED="1444451455177" ID="ID_1204243787" MODIFIED="1444451490322">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -340,6 +342,427 @@
 <node CREATED="1444451553366" ID="ID_323764409" MODIFIED="1444451554565" TEXT="Validez "/>
 <node CREATED="1444451561116" ID="ID_257864903" MODIFIED="1444451563449" TEXT="Autorizaci&#xf3;n "/>
 <node CREATED="1444451572657" ID="ID_78364360" MODIFIED="1444451578773" TEXT="Segregaci&#xf3;n de funciones"/>
+</node>
+</node>
+<node CREATED="1444560930007" ID="ID_1254769920" MODIFIED="1444561024683" VSHIFT="48">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      FRONTERAS DE LOS
+    </p>
+    <p>
+      CONTROLES DE NEGOCIO
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1444561275061" ID="ID_1981127362" MODIFIED="1444561358267">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      CONTROLES DE APLICACI&#211;N
+    </p>
+    <ul>
+      <li>
+        &#160;ACn, de Control de Aplicaci&#243;n
+      </li>
+    </ul>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1444561500993" ID="ID_350889202" MODIFIED="1444561597227">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        AC1 Preparaci&#243;n y Autorizaci&#243;n de Informaci&#243;n Fuente.
+      </li>
+      <li>
+        AC2 Recolecci&#243;n y Entrada de Informaci&#243;n Fuente
+      </li>
+      <li>
+        AC3 Chequeos de Exactitud, Integridad y Autenticidad
+      </li>
+      <li>
+        AC4 Integridad y Validez del Procesamiento
+      </li>
+      <li>
+        AC5 Revisi&#243;n de Salidas, Reconciliaci&#243;n y Manejo de Errores
+      </li>
+      <li>
+        AC6 Autenticaci&#243;n e Integridad de Transacciones
+      </li>
+    </ul>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1444560957839" ID="ID_1901522740" MODIFIED="1444560964626" TEXT="TIEMPO 1">
+<node CREATED="1444560979695" ID="ID_901893885" MODIFIED="1444560994421">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      RESPONSABILIDAD&#160;
+    </p>
+    <p>
+      DE NEGODIO
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1444561083449" HGAP="46" ID="ID_645954338" MODIFIED="1444561174376" TEXT="CONTROLES DE NEGOCIO" VSHIFT="42"/>
+<node CREATED="1444561093980" HGAP="50" ID="ID_938181557" MODIFIED="1444561177008" TEXT="REQUERIMIENTOS FUNCIONALES"/>
+<node CREATED="1444561103967" HGAP="47" ID="ID_794488551" MODIFIED="1444561180249" TEXT="REQUERIMIENTOS DE CONTROL" VSHIFT="1"/>
+</node>
+</node>
+<node CREATED="1444560964902" ID="ID_706093199" MODIFIED="1444560968382" TEXT="TIEMPO 2">
+<node CREATED="1444561005114" ID="ID_1295250380" MODIFIED="1444561011489" TEXT="RESPONSABILIDAD DE TI">
+<node CREATED="1444561124309" HGAP="29" ID="ID_754201197" MODIFIED="1444561182865" TEXT="CONTROLES GENERALES DE TI" VSHIFT="43">
+<arrowlink DESTINATION="ID_1880385736" ENDARROW="Default" ENDINCLINATION="567;0;" ID="Arrow_ID_749439136" STARTARROW="None" STARTINCLINATION="567;0;"/>
+</node>
+<node CREATED="1444561189778" ID="ID_229048562" MODIFIED="1444561195898" TEXT="DOMINIO COBIT">
+<node CREATED="1444561197323" ID="ID_712735155" MODIFIED="1444561199078" TEXT="PO"/>
+<node CREATED="1444561204383" ID="ID_945177475" MODIFIED="1444561206281" TEXT="AI"/>
+<node CREATED="1444561207128" ID="ID_208295499" MODIFIED="1444561216085">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      DS
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1444561219217" ID="ID_985824056" MODIFIED="1444561221430" TEXT="ME"/>
+</node>
+</node>
+</node>
+<node CREATED="1444560968656" ID="ID_283476389" MODIFIED="1444560971701" TEXT="TIEMPO 3">
+<node CREATED="1444561014263" ID="ID_838028742" MODIFIED="1444561020572">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      RESPONSABILIDAD&#160;
+    </p>
+    <p>
+      DE NEGODIO
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1444561399297" ID="ID_357216874" MODIFIED="1444561405514" TEXT="CONTROLES DENEGOCIO"/>
+<node CREATED="1444561308380" ID="ID_191219777" MODIFIED="1444561315737" TEXT="SERVICIOS AUTOMATIZADOS"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1444562200003" ID="ID_1137011658" MODIFIED="1444563242765" TEXT="MODELO DE MADUREZ">
+<node CREATED="1444563242730" ID="ID_1908614655" MODIFIED="1444563242730" TEXT="">
+<node CREATED="1444562233657" ID="ID_1521765633" MODIFIED="1444562255036">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      un plan de negocio
+    </p>
+    <p>
+      para mejorar y alcanzar
+    </p>
+    <p>
+      el nivel apropiado de
+    </p>
+    <p>
+      administraci&#243;n y control
+    </p>
+    <p>
+      sobre la infraestructura
+    </p>
+    <p>
+      de informaci&#243;n
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1444562287167" ID="ID_1035869469" MODIFIED="1444562477230">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      considerar el equilibrio del costo beneficio
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1444562477220" ID="ID_402415828" MODIFIED="1444562477220" TEXT="">
+<node CREATED="1444562305521" ID="ID_1054004830" MODIFIED="1444562510393">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#191;Qu&#233; est&#225; haciendo nuestra competencia
+    </p>
+    <p>
+      en la industria, y c&#243;mo estamos posicionados
+    </p>
+    <p>
+      en relaci&#243;n a ellos?
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1444562399224" ID="ID_133792625" MODIFIED="1444562499325" TEXT="&#xbf;Cu&#xe1;les son las mejores pr&#xe1;cticas aceptables en la industria, y c&#xf3;mo estamos posicionados con respecto a estas pr&#xe1;cticas?"/>
+<node CREATED="1444562411382" ID="ID_145080370" MODIFIED="1444562412389" TEXT="Con base en estas comparaciones, &#xbf;se puede decir que estamos haciendo lo suficiente?"/>
+<node CREATED="1444562422406" ID="ID_575178603" MODIFIED="1444562488123" TEXT="&#xbf;C&#xf3;mo identificamos lo que se requiere hacer para alcanzar un nivel adecuado de administraci&#xf3;n y control sobre nuestros procesos de TI?"/>
+</node>
+<node CREATED="1444562542403" ID="ID_592819295" MODIFIED="1444562542403" TEXT="">
+<node CREATED="1444562545256" ID="ID_874851430" MODIFIED="1444562571720" TEXT="Una medici&#xf3;n relativa de d&#xf3;nde se encuentra la empresa">
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1444562558596" ID="ID_1278720232" MODIFIED="1444562574148" TEXT="Una manera de decidir hacia d&#xf3;nde ir de forma eficiente">
+<icon BUILTIN="full-2"/>
+</node>
+<node CREATED="1444562566505" ID="ID_1121358887" MODIFIED="1444562577109" TEXT="Una herramienta para medir el avance contra la meta ">
+<icon BUILTIN="full-3"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1444562968704" ID="ID_899965950" MODIFIED="1444563271154" TEXT="">
+<node CREATED="1444562970791" ID="ID_482984326" MODIFIED="1444563266818" TEXT=" desde un nivel de no-existente (0) hasta un nivel de optimizado (5)">
+<node CREATED="1444563280471" HGAP="30" ID="ID_1158543396" MODIFIED="1444563295382" VSHIFT="74">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#160;Representaci&#243;n Gr&#225;fica de los Modelos de Madurez
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1444563314171" ID="ID_1515061029" MODIFIED="1444563367420">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      No existente
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="full-0"/>
+<node CREATED="1444563503009" HGAP="60" ID="ID_1556184878" MODIFIED="1444563508916" TEXT="No se aplican procesos administrativos en lo absoluto" VSHIFT="-77"/>
+</node>
+<node CREATED="1444563328966" ID="ID_1130799330" MODIFIED="1444563386623">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Inicial /&#160;&#160;Ad hoc
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="full-1"/>
+<node CREATED="1444563519872" ID="ID_45872409" MODIFIED="1444563521207" TEXT=" Los procesos son ad-hoc y desorganizados"/>
+</node>
+<node CREATED="1444563406127" ID="ID_1976785975" MODIFIED="1444563410829" TEXT="Repetible pero Intuitivo  ">
+<icon BUILTIN="full-2"/>
+<node CREATED="1444563530998" ID="ID_463377990" MODIFIED="1444563532609" TEXT="Los procesos siguen un patr&#xf3;n regular"/>
+</node>
+<node CREATED="1444563418123" ID="ID_1254379630" MODIFIED="1444563422341" TEXT="Proceso Definido ">
+<icon BUILTIN="full-3"/>
+<node CREATED="1444563543816" ID="ID_555085165" MODIFIED="1444563545973" TEXT=" Los procesos se documentan y se comunican"/>
+</node>
+<node CREATED="1444563442364" ID="ID_675672561" MODIFIED="1444563445305" TEXT="Administrado y Medible ">
+<icon BUILTIN="full-4"/>
+<node CREATED="1444563560930" ID="ID_1001795861" MODIFIED="1444563562180" TEXT="Los procesos se monitorean y se miden"/>
+</node>
+<node CREATED="1444563478569" ID="ID_1690758923" MODIFIED="1444563487948" TEXT="Optimizado">
+<icon BUILTIN="full-5"/>
+<node CREATED="1444563572414" ID="ID_965221119" MODIFIED="1444563573516" TEXT="Las buenas  LEYENDA PARA LA CALIFICACI&#xd3;N USADA   pr&#xe1;cticas se siguen y se automatizan "/>
+</node>
+</node>
+<node CREATED="1444564425351" ID="ID_1153405404" MODIFIED="1444564471620" VSHIFT="76">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Figura 15 - Tabla de Atributos de Madurez
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1444564451343" ID="ID_1281133884" MODIFIED="1444564452976" TEXT="Conciencia y  Comunicaci&#xf3;n">
+<node CREATED="1444564540043" HGAP="132" ID="ID_1417601186" MODIFIED="1444564750034" TEXT="Surge el reconocimiento de la necesidad del proceso.   &#xa;Existe comunicaci&#xf3;n espor&#xe1;dica de los problemas. " VSHIFT="6">
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1444564612676" HGAP="137" ID="ID_991470668" MODIFIED="1444564753266" VSHIFT="5">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Existe conciencia de la necesidad de actuar.&#160;&#160;&#160;
+    </p>
+    <p>
+      La gerencia comunica los problemas generales.&#160;
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="full-2"/>
+</node>
+<node CREATED="1444564649872" HGAP="135" ID="ID_1709461226" MODIFIED="1444564755882" TEXT="Existe el entendimiento de la necesidad de actuar.   &#xa;La gerencia es m&#xe1;s formal y estructurada en su comunicaci&#xf3;n. " VSHIFT="41">
+<icon BUILTIN="full-3"/>
+</node>
+<node CREATED="1444564680221" HGAP="140" ID="ID_1413817749" MODIFIED="1444564758257" TEXT="Hay entendimiento de los requerimientos completos.   &#xa;Se aplican t&#xe9;cnicas maduras de comunicaci&#xf3;n y se usan &#xa;herramientas est&#xe1;ndar de comunicaci&#xf3;n.  " VSHIFT="25">
+<icon BUILTIN="full-4"/>
+</node>
+<node CREATED="1444564723463" HGAP="138" ID="ID_797466914" MODIFIED="1444564762074" TEXT="Existe un entendimiento avanzado y a futuro de los requerimientos.   &#xa;Existe una comunicaci&#xf3;n proactiva de los problemas, basada en las tendencias, &#xa;se aplican t&#xe9;cnicas maduras de comunicaci&#xf3;n y se usan herramientas &#xa;integradas de comunicaci&#xf3;n  " VSHIFT="39">
+<icon BUILTIN="full-5"/>
+</node>
+</node>
+<node CREATED="1444564454687" ID="ID_715682111" MODIFIED="1444564468823">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Pol&#237;ticas, Est&#225;ndares y Procedimientos
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1444564902533" HGAP="95" ID="ID_1442563857" MODIFIED="1444564909136" VSHIFT="60">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Existen enfoques ad hoc hacia los procesos y las pr&#225;cticas.&#160;
+    </p>
+    <p>
+      Los procesos y las pr&#225;cticas no est&#225;n definidos
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1444564930619" HGAP="111" ID="ID_1266300725" MODIFIED="1444564966301" VSHIFT="25">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Surgen procesos similares y comunes pero en su mayor&#237;a son intuitivos
+    </p>
+    <p>
+      y parten de la experiencia individual.&#160;
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      Algunos aspectos de los procesos son repetibles debido a la
+    </p>
+    <p>
+      experiencia individual, y puede existir alguna documentaci&#243;n y
+    </p>
+    <p>
+      entendimiento informal de pol&#237;ticas y procedimientos.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1444565017606" HGAP="107" ID="ID_1522149438" MODIFIED="1444565024800" VSHIFT="25">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Surge el uso de buenas pr&#225;cticas.&#160;
+    </p>
+    <p>
+      Los procesos, pol&#237;ticas y procedimientos est&#225;n definidos y documentados para todas las actividades clave.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1444565041951" HGAP="98" ID="ID_1458914368" MODIFIED="1444565049695" VSHIFT="56">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      El proceso es s&#243;lido y completo; se aplican las mejores pr&#225;cticas internas.&#160;
+    </p>
+    <p>
+      Todos los aspectos del proceso est&#225;n documentados y son repetibles. La direcci&#243;n ha terminado y aprobado las pol&#237;ticas. Se adoptan y siguen est&#225;ndares para el desarrollo y mantenimiento.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1444565065820" HGAP="99" ID="ID_86590461" MODIFIED="1444565074335" VSHIFT="27">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Se aplican las mejores pr&#225;cticas y est&#225;ndares externos.&#160;
+    </p>
+    <p>
+      La documentaci&#243;n de procesos ha evolucionado a flujos de trabajo automatizados. Los procesos, las pol&#237;ticas y los procedimientos est&#225;n estandarizados e integrados para permitir una administraci&#243;n y mejora extremo a extremo.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1444564479882" ID="ID_58945804" MODIFIED="1444564481291" TEXT="Herramientas y  Automatizaci&#xf3;n "/>
+<node CREATED="1444564489700" ID="ID_541693601" MODIFIED="1444564490994" TEXT="Habilidades y  Experiencia "/>
+<node CREATED="1444564498157" ID="ID_116101974" MODIFIED="1444564499356" TEXT="Responsabilidad y  Rendici&#xf3;n de Cuentas "/>
+<node CREATED="1444564506984" ID="ID_1698636137" MODIFIED="1444564508119" TEXT="Establecimiento y  Medici&#xf3;n de Metas "/>
+</node>
+</node>
 </node>
 </node>
 </node>
